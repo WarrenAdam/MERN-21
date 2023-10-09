53ec216bd8b697b60851e0660e2ca444bc277a82
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-10-09 08:37:56 -0400

    updated models

--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,33 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema } = require('mongoose');
 
-// import schema from Book.js
-const bookSchema = require('./Book');
-
-const userSchema = new Schema(
-  {
-    username: {
+// NOTE: This is a subdocument schema, 
+// it won't become its own model but we'll use it as the schema
+// for the User's `savedBooks` array in User.js
+const bookSchema = new Schema({
+  authors: [
+    {
       type: String,
-      required: true,
-      unique: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    // set savedBooks to be an array of data that adheres to the bookSchema
-    savedBooks: [bookSchema],
+  ],
+  description: {
+    type: String,
+    required: true,
   },
-  // set this to use virtual below
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
-
-// hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
+  // saved book id from GoogleBooks
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
-// custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
-
-const User = model('User', userSchema);
-
-module.exports = User;
+module.exports = bookSchema;
